d24eb71f9cfac07b3cd787fa18f884a33f998841
bogdan.feher@gmail.com <bogdan.feher@gmail.com>
2025-07-22 17:18:18 -0400

    please?

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { defineConfig } from "vite"
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/pavepro-frontend/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
